8ff4e6e7a2edffcd38c6fe1fd48db377d6923abf
Atif Galamov <kalemli@gmail.com>
2019-01-08 18:17:26 +0400

    DataTables: OrderBy

--- DataList/Controllers/Api/PersonsController.cs
+++ DataList/Controllers/Api/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using DataList.Models;
 using DataList.Models.DataTables;
+using DataList.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,24 +19,26 @@ namespace DataList.Controllers.Api
         {
             var qPersons = Context.Persons.AsQueryable();
             if (!string.IsNullOrEmpty(model.search.Fullname))
-            {
                 qPersons = qPersons.Where(p => 
                     (p.Firstname + " " + p.Surname + " " + p.Lastname).Contains(model.search.Fullname));
-            }
             if (!string.IsNullOrEmpty(model.search.Code))
-            {
                 qPersons = qPersons.Where(p => p.Code.Contains(model.search.Code));
-            }
             if (!string.IsNullOrEmpty(model.search.Workplace))
-            {
                 qPersons = qPersons.Where(p => p.Workplace.Contains(model.search.Workplace));
-            }
             if (!string.IsNullOrEmpty(model.search.Address))
-            {
                 qPersons = qPersons.Where(p => p.Address.Contains(model.search.Address));
+
+            if (model.order.Count > 0)
+            {
+                var order = model.order[0];
+                string colName = model.columns[order.column].data;
+                colName = colName.Substring(0, 1).ToUpper() + colName.Substring(1);
+                qPersons = qPersons.OrderByName(colName, order.dir == "desc");
             }
+            else
+                qPersons = qPersons.OrderBy(p => p.Id);
+
             var persons = qPersons
-                .OrderBy(p => p.Id)
                 .Skip(model.start)
                 .Take(model.length)
                 .ToList();
---
+++ DataList/Helpers/QueryableHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+
+namespace DataList.Helpers
+{
+    public static class QueryableHelper
+    {
+        public static IQueryable<T> OrderByName<T>(this IQueryable<T> source, string propertyName, Boolean isDescending)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (propertyName == null) throw new ArgumentNullException("propertyName");
+
+            Type type = typeof(T);
+            ParameterExpression arg = Expression.Parameter(type, "x");
+
+            PropertyInfo pi = type.GetProperty(propertyName);
+            Expression expr = Expression.Property(arg, pi);
+            type = pi.PropertyType;
+
+            Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+            LambdaExpression lambda = Expression.Lambda(delegateType, expr, arg);
+
+            String methodName = isDescending ? "OrderByDescending" : "OrderBy";
+            object result = typeof(Queryable).GetMethods().Single(
+                method => method.Name == methodName
+                        && method.IsGenericMethodDefinition
+                        && method.GetGenericArguments().Length == 2
+                        && method.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(T), type)
+                .Invoke(null, new object[] { source, lambda });
+            return (IQueryable<T>)result;
+        }
+    }
+}
